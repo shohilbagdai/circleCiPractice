ca928ae2a1809016d754172513522b212397cd6b
shohilbagdai <shohil.bagdai@statravel.com>
2017-08-02 15:29:11 +0100

    master

--- src/main/java/com/statravel/autoqa/AdminQAApplication.java
+++ src/main/java/com/statravel/autoqa/AdminQAApplication.java
@@ -1,7 +1,9 @@
 package com.statravel.autoqa;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 /**
  * 
@@ -9,6 +11,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  *
  */
 @SpringBootApplication
+@EnableAutoConfiguration(exclude = { DataSourceAutoConfiguration.class })
 public class AdminQAApplication {
 
     /**
